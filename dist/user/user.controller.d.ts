da06e463d63c91654aeddfb2713ba456bdfd070b
User <dipali.shinde1@niveusslutions.com>
2024-12-23 02:55:13 +0530

    initial user application delete dist file

--- dist/user/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './user.service';
-import { User } from './entities/user.entity';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    createNewUser(): Promise<void>;
-    findAllUsers(): Promise<User[]>;
-    findOneUser(id: number): Promise<User>;
-}
--- dist/user/user.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
-export declare class UserService {
-    private userRepository;
-    constructor(userRepository: Repository<User>);
-    create(username: string, password: string, email: string): Promise<User>;
-    getAllUsers(): Promise<User[]>;
-    findOne(id: number): Promise<User>;
-}
